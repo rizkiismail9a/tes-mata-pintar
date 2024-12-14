04d56ccfd29052f4283b17c525bb405b69b3d4b3
rizkiismail9a <muhamadrizkiismail9a@gmail.com>
2024-12-14 17:14:50 +0700

    feat: get test history funtion

---
+++ components/common/TextBadge.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+  type: "success" | "danger";
+  label: string;
+}>();
+</script>
+
+<template>
+  <div
+    :class="`flex items-center justify-center rounded-md p-1 text-sm text-white ${type}`"
+  >
+    {{ label }}
+  </div>
+</template>
+
+<style scoped>
+.success {
+  background-color: rgb(13, 196, 13);
+}
+
+.danger {
+  background-color: red;
+}
+</style>
